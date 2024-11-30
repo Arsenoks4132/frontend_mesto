3aa7d78fb459f4b8cb74768355d0eb880caa4029
Arsenoks4132 <Danov2005@yandex.ru>
2024-11-30 11:36:22 +0300

    finish project

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,98 @@ import { initialCards } from './cards.js';
 
 // Общие функции
 
+function closeByOverlay(event) {
+    closeModal(event.target);
+}
+
+function addOverlayListener() {
+    const popup = document.querySelector('.popup_is-opened');
+    popup.addEventListener('click', closeByOverlay);
+}
+
+function removeOverlayListener() {
+    const popup = document.querySelector('.popup_is-opened');
+    popup.removeEventListener('click', closeByOverlay);
+}
+
+function closeByEscape(event) {
+    if (event.key === 'Escape') {
+        const popup = document.querySelector('.popup_is-opened');
+        closeModal(popup);
+    }
+}
+
 function openModal(popup) {
     popup.classList.add('popup_is-opened');
+
+    const popupContent = popup.querySelector('.popup__content');
+
+    document.addEventListener('keydown', closeByEscape);
+
+    popup.addEventListener('click', closeByOverlay);
+    popupContent.addEventListener('mouseleave', addOverlayListener);
+    popupContent.addEventListener('mouseenter', removeOverlayListener);
 }
 
 function closeModal(popup) {
     popup.classList.remove('popup_is-opened');
+    const popupContent = popup.querySelector('.popup__content');
+    popup.removeEventListener('click', closeByOverlay);
+    popupContent.removeEventListener('mouseleave', addOverlayListener);
+    popupContent.removeEventListener('mouseenter', removeOverlayListener);
+    document.removeEventListener('keydown', closeByEscape);
 }
+
+
+
+// Функции валидации форм
+
+function showInputError(formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    errorElement.textContent = errorMessage;
+    inputElement.classList.add('popup__input_error');
+    errorElement.classList.add('popup__error-text_active');
+};
+
+function hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+    errorElement.textContent = '';
+    inputElement.classList.remove('popup__input_error');
+    errorElement.classList.remove('popup__error-text_active');
+};
+
+function checkInputValidity(formElement, inputElement) {
+    if (inputElement.validity.valid) {
+        hideInputError(formElement, inputElement);
+    } else {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+    }
+};
+
+function hasInvalidInput(inputList) {
+    return inputList.some((element) => (!element.validity.valid))
+};
+
+function toggleButtonState(inputList, buttonElement) {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.classList.add('popup__button_inactive');
+    }
+    else {
+        buttonElement.classList.remove('popup__button_inactive');
+    }
+};
+
+function setEventListeners(formElement) {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const buttonElement = formElement.querySelector('.popup__button');
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            checkInputValidity(formElement, inputElement);
+            toggleButtonState(inputList, buttonElement);
+        });
+    });
+};
 
 
 
@@ -129,3 +214,12 @@ initialCards.forEach((item) => cardsList.append(createCard(item.name, item.link)
 profilePopup.classList.add('popup_is-animated');
 cardPopup.classList.add('popup_is-animated');
 imagePopup.classList.add('popup_is-animated');
+
+const formList = Array.from(document.querySelectorAll('.popup__form'));
+formList.forEach((formElement) => {
+    formElement.addEventListener('submit', function (evt) {
+        evt.preventDefault();
+    });
+
+    setEventListeners(formElement);
+}); 
